5da6957aa6bc80d5b058056097651a064c0f28e3
tictandess <59805493+tictandess@users.noreply.github.com>
2020-10-23 19:34:13 +0200

    Update hallo.js

--- hallo.js
+++ hallo.js
@@ -1,6 +1,6 @@
 function sayHello() {
   console.log("Hello aus Github");
-  window.location.href = "https://www.immobilienscout24.de/Suche/de/hessen/giessen-kreis/haus-kaufen?sorting=2";
+  //window.location.href = "https://www.immobilienscout24.de/Suche/de/hessen/giessen-kreis/haus-kaufen?sorting=2";
   
   setTimeout(function(){ console.log(document.querySelectorAll('[data-obid]')); }, 4000);
 
